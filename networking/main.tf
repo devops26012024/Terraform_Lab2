3ad2b58964bc7c62ea7ed3438e15d5796ee75980
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-18 10:43:23 +0530

    networking.tf

---
+++ networking/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-training-196"
+    key            = "networking/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "networking" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "networking-vpc"
+  }
+}
