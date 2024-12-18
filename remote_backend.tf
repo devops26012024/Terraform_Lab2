3ec16a60d737c1f8791c0f6b6762ed522f9bdaaf
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-18 11:04:39 +0530

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform_demo1"  
+    dynamodb_table = "terraform_demo"  
   }
 }
